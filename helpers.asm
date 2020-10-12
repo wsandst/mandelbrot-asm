e58979da67282f3bcba9ac1f400bac9d50c855a0
williamsandst <williamsandstrom99@hotmail.com>
2020-10-12 11:21:43 +0200

    Added helpful macros in the helpers.asm file

---
+++ helpers.asm
@@ -0,0 +1,59 @@
+section .bss
+        numberDigits resq 64
+        
+; Macros
+; Exit the program
+%macro exit 1
+        mov       rax, SYS_EXIT           ; system call for exit
+        xor       rdi, %1                 ; exit code %1
+        syscall                           ; invoke operating system to exit
+%endmacro
+
+; Print a message to stdout, using the register msg pointer rsi and length rdx
+%macro print 2
+        mov       rsi, %1                 ; address of string to output
+        mov       rdx, %2                 ; number of bytes
+        mov       rax, SYS_WRITE          ; system call for write
+        mov       rdi, STDOUT             ; file handle 1 is stdout
+        syscall                           ; invoke operating system to do the write
+%endmacro
+
+%macro printInt 1
+; Prints the value of RAX as a base 10 number
+    mov rax, %1
+    mov r8, numberDigits
+    mov r10, numberDigits ; Start of digit pointer
+    %%isolateDigitsLoop:
+        ; rax = rax / 10. rdx is the remainder
+        mov rcx, 10
+        xor rdx,rdx             ; clear rdx for correct remainder output
+        div rcx 
+
+        ; move the remainder rdx into the digit array
+        add rdx, '0'            ; Add char '0' to get the integer in ASCII
+        mov [r8], rdx
+        inc r8                  ; Increment the digit pointer
+
+        cmp rax, 0
+        jne %%isolateDigitsLoop   ; Keep looping while there still are digits to process
+
+    %%printDigits:
+        ; Print the value of the r8 pointer
+        mov rsi, r8             ; Give the address of the number pointer
+        mov rdx, 1
+        mov rax, SYS_WRITE      ; system call for write
+        mov rdi, STDOUT         ; file handle stdout
+        syscall
+
+        dec r8          ; Decrement pointer pos
+
+        cmp r8, r10
+        jge %%printDigits
+%endmacro
+
+STDIN equ 0
+STDOUT equ 1
+
+SYS_READ equ 0
+SYS_WRITE equ 1
+SYS_EXIT equ 60
--- mandelbrot.asm
+++ mandelbrot.asm
@@ -3,67 +3,43 @@
 ;
 ; ----------------------------------------------------------------------------------------
 
+; Include helper file
+%include "helpers.asm"
 
+; Main program
 section   .data
         msg:  db        "Generating mandelbrot"      ; note the newline at the end
         msglen:  equ $-msg
         newline: db 10
+        imageheader: db "Hello, world!"
+        outputfilename: db "output.ppm", 0
+        outputfilenamelen: equ $-outputfilename
 
 section .bss
-        numberDigits resq 64
+        imageArray resb 64*64*3
+        filehandle resq 1
 
 section   .text
         global    _main
 
-        ; Print "Fibonnaci:"
 main:   
-        mov       rsi, msg                ; address of string to output
-        mov       rdx, msglen             ; number of bytes
-        call _print
+        print msg, msglen
 
-        ; Print newline
-        mov       rsi, newline            ; address of string to output
-        mov       rdx, 1                  ; number of bytes
-        call _print
+        print newline, 1
 
-        ; Exit
-        mov       rax, 60                 ; system call for exit
-        xor       rdi, rdi                ; exit code 0
-        syscall                           ; invoke operating system to exit
+        printInt 10
 
-_print: ; Print a message to stdout, using the register msg pointer rsi and length rdx
-        mov       rax, 1                  ; system call for write
-        mov       rdi, 1                  ; file handle 1 is stdout
-        syscall                           ; invoke operating system to do the write
-        ret
+        print newline, 1
 
-_printInt: ; Prints the value of RAX as a base 10 number
-    mov r8, numberDigits
-    mov r10, numberDigits ; Start of digit pointer
-    isolateDigitsLoop:
-        ; rax = rax / 10. rdx is the remainder
-        mov rcx, 10
-        xor rdx,rdx             ; clear rdx for correct remainder output
-        div rcx 
+        exit 0
 
-        ; move the remainder rdx into the digit array
-        add rdx, '0'            ; Add char '0' to get the integer in ASCII
-        mov [r8], rdx
-        inc r8                  ; Increment the digit pointer
-
-        cmp rax, 0
-        jne isolateDigitsLoop   ; Keep looping while there still are digits to process
-
-    printDigits:
-        ; Print the value of the r8 pointer
-        mov rsi, r8     ; Give the address of the number pointer
-        mov rdx, 1
-        mov rax, 1      ; system call for write
-        mov rdi, 1      ; file handle 1 is stdout
+_openfile:
+        mov rax, 2 ; system call for open
+        mov rdi, outputfilename
+        mov rsi, 577 ; flags
+        mov rdx, 0644o ; mode. read and write
         syscall
+        mov [filehandle], rax
 
-        dec r8          ; Decrement pointer pos
-
-        cmp r8, r10
-        jge printDigits
-    ret
+_writeheader:
+        mov rax, 4         ;syscall 4 - write()
